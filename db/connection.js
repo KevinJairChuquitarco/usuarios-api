7659f50b382961dc17ccc13665aaa6293684fdaf
KevinJair11 <kevinchuquitarco11@gmail.com>
2025-06-21 11:36:17 -0500

    creación de la base de datos

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,28 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.postgres_uri
+  connectionString: process.env.postgres_uri,
 });
 
+async function setupDatabase() {
+  try {
+    console.log("Contectando a la base de datos....");
+    await pool.query("Select Now()");
+    console.log("Conexión exitosa");
+    await pool.query(`
+            create table usuario (
+            id serial primary key,
+            nombre varchar(50) not null,
+            email varchar(50) unique not null,
+            contrasena varchar(100) not null
+            );
+            `);
+    console.log("Base de datos Creada con éxito");
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+setupDatabase();
 module.exports = pool;
